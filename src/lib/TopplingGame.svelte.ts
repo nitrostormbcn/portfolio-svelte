a89dc7c923514a92a292d9ce0c873cf258f06bc6
Pau Martinez <zenit.uap@gmail.com>
2026-02-28 23:20:08 +0100

    Refactor topple

--- src/lib/TopplingGame.svelte.ts
+++ src/lib/TopplingGame.svelte.ts
@@ -3,10 +3,13 @@ import { mount } from 'svelte';
 
 export class TopplingGame {
 	currentLevel: number = 0;
-	levelDifficulty = [1, 2, 3, 5, 10];
-	content: HTMLDivElement;
+	levelDifficulty = [10, 2, 3, 5, 10];
+	currentLevelProgress: number = 0;
+	paragraphs: Element[];
+	initialParagraphs: Element[];
 	constructor(content: HTMLDivElement) {
-		this.content = content;
+		this.paragraphs = Array.from(content.children);
+		this.initialParagraphs = [...this.paragraphs];
 	}
 
 	startGame() {
@@ -19,50 +22,21 @@ export class TopplingGame {
 	}
 
 	private splitTextContentIn(sections: number) {
-		const paragraphs = Array.from(this.content.children);
-		const totalLetters = paragraphs.reduce(
-			(previous, current) => previous + current.textContent.length,
-			0
-		);
-		let splits = Array.from({ length: sections }, () => TopplingGame.randomIntRange(totalLetters));
-		while (new Set(splits).size !== splits.length) {
-			splits = Array.from({ length: sections }, () => TopplingGame.randomIntRange(totalLetters));
-		}
-		splits.sort((a, b) => a - b);
-
-		const relativeSplits: number[][] = Array.from({ length: paragraphs.length }, () => []);
-
-		for (let split of splits) {
-			for (let index = 0; index < paragraphs.length; index++) {
-				const paragraph = paragraphs[index];
-				const elementLength = paragraph.textContent.length;
-				if (split <= elementLength) {
-					relativeSplits[index].push(split);
-					break;
-				}
-				split -= paragraph.textContent.length;
-			}
-		}
+		const relativeSplits = this.generateRelativeSplits(sections);
 
 		for (let index = 0; index < relativeSplits.length; index++) {
 			const elementSplits = relativeSplits[index];
-			if (elementSplits.length === 0) {
-				continue;
-			}
-			const paragraph = paragraphs[index];
+			const paragraph = this.paragraphs[index];
+			if (elementSplits.length === 0) continue;
+
 			const text = paragraph.textContent;
 			paragraph.textContent = '';
 			const e = document.createElement('span');
+			elementSplits[0] = Utils.slideSplitOnSpaces(elementSplits[0], text);
 			e.textContent = text.slice(0, elementSplits[0]);
 			paragraph.appendChild(e);
 			for (let index = 0; index < elementSplits.length; index++) {
-				let split = elementSplits[index];
-
-				if (text.charAt(split) === ' ') {
-					const t1 = text.charAt(split - 1);
-					split = t1 !== '' ? split - 1 : split + 1;
-				}
-
+				const split = Utils.slideSplitOnSpaces(elementSplits[index], text);
 				const e = document.createElement('span');
 				e.textContent = text.slice(split + 1, elementSplits[index + 1]);
 				mount(ToppleButton, {
@@ -74,7 +48,48 @@ export class TopplingGame {
 		}
 	}
 
-	private static randomIntRange(max: number) {
+	private generateAbsoluteSplits(num: number): number[] {
+		const totalLetters = this.paragraphs.reduce(
+			(previous, current) => previous + current.textContent.length,
+			0
+		);
+		let splits = Array.from({ length: num }, () => Utils.randomIntRange(totalLetters));
+		while (new Set(splits).size !== splits.length) {
+			// Reroll if there are repeated splits
+			splits = Array.from({ length: num }, () => Utils.randomIntRange(totalLetters));
+		}
+		return splits.sort((a, b) => a - b);
+	}
+
+	private generateRelativeSplits(sections: number): number[][] {
+		const splits = this.generateAbsoluteSplits(sections);
+		const relativeSplits: number[][] = Array.from({ length: this.paragraphs.length }, () => []);
+		for (let split of splits) {
+			for (let index = 0; index < this.paragraphs.length; index++) {
+				const paragraph = this.paragraphs[index];
+				const elementLength = paragraph.textContent.length;
+				if (split <= elementLength) {
+					relativeSplits[index].push(split);
+					break;
+				}
+				split -= paragraph.textContent.length;
+			}
+		}
+		return relativeSplits;
+	}
+}
+
+class Utils {
+	public static randomIntRange(max: number) {
 		return Math.floor(Math.random() * max);
+	}
+
+	public static slideSplitOnSpaces(split: number, text: string) {
+		if (text.charAt(split) === ' ') {
+			const t1 = text.charAt(split - 1);
+			console.log(split);
+			return t1 !== '' ? split - 1 : split + 1;
+		}
+		return split;
 	}
 }
