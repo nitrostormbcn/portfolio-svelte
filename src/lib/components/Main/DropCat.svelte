b6c1eac752f69b3ec4071168d7b868660082d0d8
Pau Martinez <zenit.uap@gmail.com>
2026-03-08 21:14:03 +0100

    Drag cat

--- src/lib/components/Main/DropCat.svelte
+++ src/lib/components/Main/DropCat.svelte
@@ -2,9 +2,20 @@
 	import SolidSimulation, { Solid, startSimulation } from './SolidSimulation.svelte';
 
 	let free = $state(false);
+	let isDragging = $state(false);
 	let cat = new Solid(0, 0);
 
-	function onTap() {
+	function onDragStop() {
+		if (isDragging) {
+			isDragging = false;
+			cat.unsetKinematic();
+			startSimulation([cat]);
+		}
+	}
+
+	function onDragStart(e: PointerEvent) {
+		isDragging = true;
+		e.preventDefault();
 		if (!cat.elementReference) {
 			console.log('No element');
 			return;
@@ -12,15 +23,27 @@
 		const rect = cat.elementReference.getBoundingClientRect();
 		free = true;
 		cat.setRectangle(rect);
-		startSimulation([cat]);
+		cat.setKinematic();
+		cat.draw();
+	}
+
+	function onDrag(e: PointerEvent) {
+		if (!isDragging) {
+			return;
+		}
+		e.preventDefault();
+		cat.setPosition(e.clientX, e.clientY);
+		cat.draw();
 	}
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <span
 	bind:this={cat.elementReference}
-	onpointerup={onTap}
+	onpointerdown={onDragStart}
 	class=" {free ? 'absolute top-0 left-0' : 'inline'} text-xl lg:text-3xl">😼</span
 >
 
 <SolidSimulation></SolidSimulation>
+
+<svelte:window onpointermove={onDrag} onpointerup={onDragStop} />
--- src/lib/components/Main/SolidSimulation.svelte
+++ src/lib/components/Main/SolidSimulation.svelte
@@ -1,7 +1,11 @@
 <script lang="ts" module>
+	import { draw } from 'svelte/transition';
+
 	const g = 10;
 	let groundY = $state(0);
-	let t0 = 0;
+	let wallX = $state(0);
+	let t0: number;
+	let isSimulationRunning = false;
 
 	export class Solid {
 		x = $state(0);
@@ -10,10 +14,25 @@
 		vy = 0;
 		w = 0;
 		h = 0;
+		t0: number;
+		isKinematic = false;
 		elementReference = $state<HTMLElement>();
 		constructor(x0: number, y0: number) {
 			this.x = x0;
 			this.y = y0;
+			this.t0 = Date.now();
+		}
+
+		setPosition(x: number, y: number) {
+			if (this.isKinematic) {
+				const tf = Date.now();
+				const dt = (tf - this.t0) / 1000;
+				this.vx = (x - this.x) / dt;
+				this.vy = (y - this.y) / dt;
+				this.t0 = tf;
+			}
+			this.x = x;
+			this.y = y;
 		}
 
 		setPositionAndSize(x: number, y: number, w: number, h: number) {
@@ -26,34 +45,71 @@
 		setRectangle(rect: DOMRect) {
 			this.setPositionAndSize(rect.x, rect.y, rect.width, rect.height);
 		}
+
+		draw() {
+			if (this.elementReference) {
+				this.elementReference.style.transform = 'translate(' + this.x + 'px,' + this.y + 'px)';
+			}
+		}
+
+		setKinematic() {
+			this.isKinematic = true;
+		}
+
+		unsetKinematic() {
+			this.isKinematic = false;
+		}
 	}
 
 	export function startSimulation(solids: Solid[]) {
+		if (isSimulationRunning) {
+			return;
+		}
+		nextFrame(solids);
+		isSimulationRunning = true;
+	}
+
+	function nextFrame(solids: Solid[]) {
 		requestAnimationFrame((tf) => {
 			mainLoop(tf, solids);
 		});
 	}
 
 	function mainLoop(tf: DOMHighResTimeStamp, solids: Solid[]) {
+		if (!t0) {
+			t0 = tf;
+			nextFrame(solids);
+		}
+		const dt = (tf - t0) / 1000;
 		solids.forEach((o) => {
-			let dt = (tf - t0) / 1000;
-
+			if (o.isKinematic) {
+				return;
+			}
 			o.vy = o.vy + g + dt;
+			o.x = o.x + o.vx * dt;
 			o.y = o.y + o.vy * dt;
-
 			if (o.y + o.h >= groundY && o.vy >= 0) {
-				o.vy = -0.6 * o.vy;
+				o.vy = -0.4 * o.vy;
+				o.vx = 0.8 * o.vx
 				o.y = groundY - o.h;
 			}
-			if (o.elementReference) {
-				o.elementReference.style.transform = 'translate(' + o.x + 'px,' + o.y + 'px)';
+			if (o.x + o.w >= wallX && o.vx >= 0) {
+				o.vx = -0.4 * o.vx;
+				o.x = wallX - o.w;
 			}
-			t0 = tf;
-			requestAnimationFrame((tf) => {
-				mainLoop(tf, solids);
-			});
+			if (o.x <= 0 && o.vx <= 0) {
+				o.vx = -0.8 * o.vx;
+				o.x = 0;
+			}
+			if (o.y <= 0 && o.vy <= 0) {
+				o.vy = -0.8 * o.vy;
+				o.y = 0;
+			}
+			o.draw();
 		});
+		t0 = tf;
+		nextFrame(solids);
 	}
 </script>
 
-<svelte:window bind:innerHeight={groundY} />
+<svelte:window bind:innerHeight={groundY} bind:innerWidth={wallX} />
