93e99d38b7b2ced8cd03888ce9ab6d98ed66d264
Pau Martinez <zenit.uap@gmail.com>
2026-03-08 18:42:04 +0100

    Add cat drop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
+<script lang="ts">
+	import DropCat from '$lib/components/Main/DropCat.svelte';
+</script>
+
 <div class="content text-container my-auto">
 	<h1 class="mb-10 text-2xl lg:text-7xl">
 		This is <span style="color: var(--color-primary);">Nitrostorm</span>'s project vault!
 	</h1>
 	<p class="text-xl lg:text-3xl">
-		You'll find some cool code projects, some art I made, and maybe some hidden cats 😼
+		You'll find some cool code projects, some art I made, and maybe some hidden cats<DropCat
+		></DropCat>
 	</p>
 </div>
+
+<div class="absolute right-0 bottom-0 m-5 text-3xl">📦</div>
 
 <style>
 	.content {
